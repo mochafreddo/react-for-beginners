47d885c889517799ec40987b15e3bcffe367e5d0
mochafreddo <corc411@gmail.com>
2023-12-27 02:43:19 +0900

    Refactor App component to fetch and display movies

--- src/App.js
+++ src/App.js
@@ -1,77 +1,39 @@
 import { useEffect, useState } from "react";
 
-async function fetchCoins() {
-  const response = await fetch("https://api.coinpaprika.com/v1/tickers");
-  if (!response.ok) {
-    throw new Error(`HTTP error! status: ${response.status}`);
-  }
-  const json = await response.json();
-  return json;
-}
-
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [purchasableCoinQuantity, setPurchasableCoinQuantity] = useState(0);
-  const [selectedCoin, setSelectedCoin] = useState(null);
-
+  const [movies, setMovies] = useState([]);
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`,
+      )
+    ).json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
   useEffect(() => {
-    fetchCoins()
-      .then((json) => {
-        setLoading(false);
-        setCoins(json);
-        setSelectedCoin(json[0]);
-      })
-      .catch((error) => {
-        console.error("Error fetching coins: ", error);
-        setLoading(false);
-      });
+    getMovies();
   }, []);
-
-  const handlePurchasableCoinQuantityChange = (event) => {
-    const value = event.target.value;
-    if (value >= 0) {
-      setPurchasableCoinQuantity((prevQuantity) => value);
-    }
-  };
-
-  const handleCoinSelect = (event) => {
-    const coin = coins.find((coin) => coin.name === event.target.value);
-    setSelectedCoin((prevCoin) => coin);
-  };
-
   return (
     <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
-        <strong>Loading...</strong>
+        <h1>Loading...</h1>
       ) : (
-        <>
-          <div>
-            <label>Please enter the amount of USD you have: </label>
-            <input
-              onChange={handlePurchasableCoinQuantityChange}
-              value={purchasableCoinQuantity}
-              type="number"
-            />
-          </div>
-          <div>
-            <select onChange={handleCoinSelect}>
-              {coins.map((coin) => (
-                <option key={coin.id} value={coin.name}>
-                  {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-                </option>
-              ))}
-            </select>
-          </div>
-          <div>
-            {selectedCoin
-              ? `With your current balance, you can purchase ${
-                  purchasableCoinQuantity / selectedCoin.quotes.USD.price
-                } of that coin.`
-              : null}
-          </div>
-        </>
+        <div>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img src={movie.medium_cover_image} />
+              <h2>{movie.title}</h2>
+              <p>{movie.summary}</p>
+              <ul>
+                {movie.genres.map((g) => (
+                  <li key={g}>{g}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
