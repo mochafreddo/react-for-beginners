be2f97d2975ab014d4079ab898735f0641943938
mochafreddo <corc411@gmail.com>
2023-12-27 01:31:44 +0900

    Add coin fetching, coin selection, and coin quantity calculation

--- src/App.js
+++ src/App.js
@@ -1,29 +1,77 @@
 import { useEffect, useState } from "react";
 
+async function fetchCoins() {
+  const response = await fetch("https://api.coinpaprika.com/v1/tickers");
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const json = await response.json();
+  return json;
+}
+
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [purchasableCoinQuantity, setPurchasableCoinQuantity] = useState(0);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+
   useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
+    fetchCoins()
       .then((json) => {
         setLoading(false);
         setCoins(json);
+        setSelectedCoin(json[0]);
+      })
+      .catch((error) => {
+        console.error("Error fetching coins: ", error);
+        setLoading(false);
       });
   }, []);
+
+  const handlePurchasableCoinQuantityChange = (event) => {
+    const value = event.target.value;
+    if (value >= 0) {
+      setPurchasableCoinQuantity((prevQuantity) => value);
+    }
+  };
+
+  const handleCoinSelect = (event) => {
+    const coin = coins.find((coin) => coin.name === event.target.value);
+    setSelectedCoin((prevCoin) => coin);
+  };
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
+        <>
+          <div>
+            <label>Please enter the amount of USD you have: </label>
+            <input
+              onChange={handlePurchasableCoinQuantityChange}
+              value={purchasableCoinQuantity}
+              type="number"
+            />
+          </div>
+          <div>
+            <select onChange={handleCoinSelect}>
+              {coins.map((coin) => (
+                <option key={coin.id} value={coin.name}>
+                  {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            {selectedCoin
+              ? `With your current balance, you can purchase ${
+                  purchasableCoinQuantity / selectedCoin.quotes.USD.price
+                } of that coin.`
+              : null}
+          </div>
+        </>
       )}
     </div>
   );
