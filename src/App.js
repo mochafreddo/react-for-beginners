7eb24376e9c9ad2311d4fdb9906c18b24e583229
mochafreddo <corc411@gmail.com>
2023-12-28 23:50:22 +0900

    Update react-router-dom version and add link to movie title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
 function App() {
-  return null;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
